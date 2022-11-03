d5463dcfbd4313804f72f6340dd1aec480dbf625
relivnd <hej+github@tobiasvones.ch>
2022-11-03 16:14:57 +0100

    init commit

---
+++ src/main/java/ch/relivnd/arvin/ArvinApplication.java
@@ -0,0 +1,13 @@
+package ch.relivnd.arvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArvinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArvinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/relivnd/arvin/ArvinApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.relivnd.arvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArvinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
